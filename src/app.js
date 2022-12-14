0d26449a9deb2df3f67db69b8e0e9624773b6c3a
Josh Bamford <joshbamford09@gmail.com>
2022-12-14 21:44:32 +0000

    create artist section complete(almost)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 /* eslint-disable semi */
 const express = require('express')
+const artistRouter = require('./routes/artist')
 
 const app = express()
 
 app.use(express.json())
 
-module.exports = app
+app.use('/artist', artistRouter)
 
-app.get('/', (req, res) => {
-  res.send({ response: 'Hello world' })
-})
+module.exports = app
--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -1,16 +1,26 @@
 const { expect } = require('chai')
 const request = require('supertest')
+const db = require('../src/db/index')
 const app = require('../src/app')
 
-describe('Create artist', () => {
+describe('create artist', () => {
   describe('/artist', () => {
     describe('POST', () => {
       it('creates a new artist in the database', async () => {
-        const res = await (await request(app).post('/artist')).send({
-          name: 'Tame Impala',
-          genre: 'rock'
+        const { status, body } = await request(app).post('/artists').send({
+          name: 'J Cole',
+          genre: 'rap'
         })
-        expect(res.status).to.equal(201)
+
+        expect(status).to.equal(201)
+        expect(body.name).to.equal('J Cole')
+        expect(body.genre).to.equal('rap')
+
+        const { rows: [artistData] } = await db.query(
+            `SELECT * FROM Artists WHERE id = ${body.id}`
+        )
+        expect(artistData.name).to.equal('J Cole')
+        expect(artistData.genre).to.equal('rap')
       })
     })
   })
